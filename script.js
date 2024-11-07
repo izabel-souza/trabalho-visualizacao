95f3c64e25b4e9de63d3217bcfff8a4c586f3b97
Izabel Soares <izabelcssoares@gmail.com>
2024-11-07 12:47:55 -0300

    scatterplot graphic added

--- script.js
+++ script.js
@@ -25,8 +25,8 @@ class GraficoBarras {
 
     async carregaArquivo(file) {
         this.barras = await d3.csv(file, d => ({
-            cat: d.nome,  // Nome correto do campo
-            valor: +d.vendas,    // Nome correto do campo
+            cat: d.nome,
+            valor: +d.vendas,
         }));
     }
 
@@ -69,6 +69,91 @@ class GraficoBarras {
             .attr("fill", "#4a90a0");
     }
 }
+
+//---------------------------------------------------------
+
+class Scatterplot {
+
+    constructor(configuracao) {
+        this.configuracao = configuracao;
+ 
+        this.svg = null;
+        this.margens = null;
+ 
+        this.escalaX = null;
+        this.escalaY = null;
+ 
+        this.circulos = [];
+ 
+        this.criaSvg();
+        this.criaMargens();
+    }
+ 
+    criaSvg() {
+        this.svg = d3.select(this.configuracao.div)
+            .append("svg")
+            .attr("width", this.configuracao.width + this.configuracao.left + this.configuracao.right)
+            .attr("height", this.configuracao.height + this.configuracao.top + this.configuracao.bottom);
+    }
+ 
+    criaMargens() {
+        this.margens = this.svg
+            .append("g")
+            .attr("transform", `translate(${this.configuracao.left},${this.configuracao.top})`);
+    }
+ 
+    async carregaArquivo(file) {
+        this.circulos = await d3.csv(file, d => ({
+            cx: +d.ano,            // Atributo 'ano' do CSV para o eixo X
+            cy: +d.vendas,         // Atributo 'vendas' do CSV para o eixo Y
+            r: 4                   // Raio fixo para os pontos no gráfico
+        }));
+    }
+ 
+    criaEscalas() {
+        const xExtent = d3.extent(this.circulos, d => d.cx);
+        const yExtent = d3.extent(this.circulos, d => d.cy);
+ 
+        this.escalaX = d3.scaleLinear()
+            .domain(xExtent)
+            .nice()
+            .range([0, this.configuracao.width]);
+ 
+        this.escalaY = d3.scaleLinear()
+            .domain([0, yExtent[1]])
+            .nice()
+            .range([this.configuracao.height, 0]);
+    }
+ 
+    criaEixos() {
+        const eixoX = d3.axisBottom(this.escalaX)
+        .ticks(10)
+        .tickFormat(d3.format("d"));
+        const eixoY = d3.axisLeft(this.escalaY).ticks(10);
+ 
+        this.margens
+            .append("g")
+            .attr("transform", `translate(0, ${this.configuracao.height})`)
+            .call(eixoX);
+ 
+        this.margens
+            .append("g")
+            .call(eixoY);
+    }
+ 
+    carregaCirculos() {
+        this.margens.selectAll("circle")
+            .data(this.circulos)
+            .join("circle")
+            .attr("cx", d => this.escalaX(d.cx))
+            .attr("cy", d => this.escalaY(d.cy))
+            .attr("r", d => d.r)
+            .attr("fill", "#4a90a0");
+    }
+ }
+
+
+//---------------------------------------------------------
 
 // class Scatterplot {
 //     constructor(configuracao) {
@@ -231,23 +316,26 @@ async function main() {
     let barras = { div: "#barras", width: 1000, height: 500, top: 30, left: 90, bottom: 200, right: 30 };
     let eixoBarras = new GraficoBarras(barras);
     await eixoBarras.carregaArquivo('jogos.csv');
-
+    
     eixoBarras.criaEscalas();
     eixoBarras.criaEixos();
     eixoBarras.carregaBarras();
 
-    // let dispersao = { div: "#dispersao", width: 800, height: 600, top: 30, left: 50, bottom: 30, right: 30 };
-    // let eixoDispersao = new Scatterplot(dispersao);
-    // await eixoDispersao.carregaArquivo("../00 - datasets/superstore.csv");
+//------------------------------------------------------
+
+    let dispersao = { div: "#dispersao", width: 1000, height: 500, top: 30, left: 90, bottom: 200, right: 30 };
+    let eixoDispersao = new Scatterplot(dispersao);
+    await eixoDispersao.carregaArquivo('jogos.csv');
+
+    eixoDispersao.criaEscalas();
+    eixoDispersao.criaEixos();
+    eixoDispersao.carregaCirculos();
+
+//------------------------------------------------------
 
     // let calor = { div: "#calor", width: 800, height: 600, top: 30, left: 50, bottom: 30, right: 30 };
     // let eixoCalor = new MapaDeCalor(calor);
     // await eixoCalor.carregaArquivo("../00 - datasets/superstore.csv");
-
-    // eixoDispersao.criaEscalas();
-    // eixoDispersao.criaEixos();
-    // eixoDispersao.carregaCirculos();
-
 
     // eixoCalor.criaEscalas();
     // eixoCalor.criaEixos();
